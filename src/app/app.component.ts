dc0e103b55f58c3afa3c6cd660c21e70535ebbde
Deepu <Deepthi@gmail.com>
2019-03-29 14:05:28 -0400

    sample test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragEnd, CdkDragStart, CdkDragMove } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-exam';
+  isAdd =  false;
+  isSub =  false;
+  isFoo =  false;
+  isDoSo = false;
+  onDragEnded(event: CdkDragEnd, operation: string) {
+    console.log('drag end', operation);
+    switch (operation) {
+      case 'Add':
+        this.isAdd = true;
+        this.isSub = false;
+        this.isFoo = false;
+        this.isDoSo = false;
+        break;
+      case 'Sub':
+      this.isAdd = false;
+      this.isSub = true;
+      this.isFoo = false;
+      this.isDoSo = false;
+      break;
+      case 'Foo':
+      this.isAdd = false;
+      this.isSub = false;
+      this.isFoo = true;
+      this.isDoSo = false;
+      break;
+      case 'doSO':
+      this.isAdd = false;
+      this.isSub = false;
+      this.isFoo = false;
+      this.isDoSo = true;
+      break;
+      default:
+      break;
+    }
+    event.source.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatNativeDateModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule,
+    BrowserAnimationsModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
